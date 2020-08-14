1e8182402313fd24496f60df286b8df2520e9618
Telepathic Programming School <t1m9m.com@gmail.com>
2020-08-14 10:44:12 +0600

    CRUD implemented

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 // import
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
 // Book model
 type Book struct {
-	ID     int `json:"id"`
+	ID     int    `json:"id"`
 	Title  string `json:"title"`
 	Author string `json:"author"`
 	Year   string `json:"year"`
@@ -20,11 +23,70 @@ var books []Book
 func main() {
 	router := mux.NewRouter()
 
+	books = append(books, Book{ID: 1, Title: "Golang pointers", Author: "Mr. Golang", Year: "2010"},
+		Book{ID: 2, Title: "Goroutines", Author: "Mr. Goroutine", Year: "2011"},
+		Book{ID: 3, Title: "Golang routers", Author: "Mr. Router", Year: "2012"},
+		Book{ID: 4, Title: "Golang concurrency", Author: "Mr. Currency", Year: "2013"},
+		Book{ID: 5, Title: "Golang good parts", Author: "Mr. Good", Year: "2014"})
+
 	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get all books is called")
+	// log.Println("Get all books is called")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	i, _ := strconv.Atoi(params["id"])
+
+	// log.Println(reflect.TypeOf(params["id"]));
+	// log.Println(reflect.TypeOf(i));
+
+	for _, book := range books {
+		if book.ID == i {
+			json.NewEncoder(w).Encode(&book)
+		}
+	}
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	books = append(books, book)
+	json.NewEncoder(w).Encode(books)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	for i, item := range books {
+		if item.ID == book.ID {
+			books[i] = book
+		}
+	}
+
+	json.NewEncoder(w).Encode(books)
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	for i, item := range books {
+		if item.ID == id {
+			books = append(books[:i], books[i+1:]...)
+		}
+	}
+
+	json.NewEncoder(w).Encode(books)
 }
